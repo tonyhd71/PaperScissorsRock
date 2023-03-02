7e19db6baaf0793a81727d89a4b8885be624dddd
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-03-02 11:24:00 -0600

    streamlined gui

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -34,8 +34,10 @@
     let selectedObj;
     let playerPts = 0;
     let comPoints =0;
+    let indicateDraw = 'this match is a draw';
     const playerScoreHtml = document.getElementById("#displayPlayerScore");
     const compScoreHtml = document.getElementById("#displayComputerScore");
+    const drawHtml = document.getElementById("#indicateDraw");
     function roundIsDraw() {
         //roundResult.innerHTML = 'This round is a Draw';
         console.log("tie");
@@ -56,71 +58,72 @@
             playerPts = parseInt(playerPts);
             playerPts = playerPts + 1;
             playerScoreHtml.innerHTML = playerPts;
+            playerScoreHtml.style.color= 'green';
             console.log("Player has " + playerPts);
         } else if (itemReturnValue == 2) {
             comPoints = parseInt(comPoints);
             comPoints = comPoints + 1;
             compScoreHtml.innerHTML = comPoints;
+            compScoreHtml.style.color= 'red';
             console.log("Computer has " + comPoints);
             
         } else if (itemReturnValue == 3) {
-            //console.log("tie");
-            roundIsDraw();
+            console.log("tie");
+            //drawHtml.textContent = indicateDraw;
         }
         if (playerPts >= 5) {
-            /*
-            displayResult.textContent = 'You have Won';
-            displayResult.style.backgroundColor = 'green';
-            displayResult.style.color = 'white';
-            */
             playAgain();
-            console.log("You have won");
+            //console.log("You have won");
             informPlayerOfResult();
         } else if (comPoints >= 5) {
-            /*
-            displayResult.textContent = 'You have Won';
-            displayResult.style.backgroundColor = 'red';
-            displayResult.style.color = 'white';
-            */
             playAgain();
-            console.log("You have lost");
             informPlayerOfResult();
         }
     } 
     let roundResult = document.getElementById("#roundResult");
-    //function ()
     function getChosenItem(plyr, comp) {
         //PLAYER WINS RETURN 1, COMPUTER WINS RETURN 2, DRAW RETURN 3
         if (plyr == comp) {
             console.log("This match is a draw");
-            //roundResult.innerHTML = 'This round is a draw';
+            progressText.textContent = 'This match is a Draw';
             //INSERT ROUND RESULTS HERE
             return 3;
         } else if (plyr =="rock" && comp =="paper") {
             console.log("rock loses to paper");
+            progressText.textContent = 'Rock loses to Paper';
+            drawHtml.textContent = '';
             return 2;
         } else if (plyr =="rock" &&  comp =="scissors") {
             console.log("rock beats scissors");
+            progressText.textContent = 'Rock beats Scissors';
+            drawHtml.textContent = '';
             return 1;
         } else if (plyr =="paper" && comp =="scissors") {
             console.log("paper loses  to scissors");
+            progressText.textContent = 'Paper loses to Scissors';
+            drawHtml.textContent = '';
             return 2;
         } else if (plyr =="paper" && comp =="rock") {
             console.log("paper beats rock");
+            progressText.textContent = 'Paper beats Rock';
+            drawHtml.textContent = '';
             return 1;
         } else if (plyr =="scissors" && comp =="rock") {
             console.log("scissors loses to rock");
+            progressText.textContent = 'Scissors loses to Paper';
+            drawHtml.textContent = '';
             return 2;
         } else if (plyr =="scissors" && comp =="paper") {
             console.log("scissors beats paper");
+            drawHtml.textContent = '';
+            progressText.textContent = 'Scissors beats Paper';
             return 1;
         } else if (plyr == null || comp == null) {
             console.log("EXCEPTION");
             return 4;
         }        
     }
-    //lost
-    const progressText = document.getElementById("trackProgress");
+    const progressText = document.getElementById("#trackProgress");
     const items = document.querySelectorAll(".imgs");   
     for (var i = 0 ; i < items.length; i++) {
         items[i].addEventListener('click', function (e) {
@@ -128,11 +131,11 @@
          let playerChoice =retrievePlayerChoice(selectedObj);
          let computerChoice = getComputerChoice();
          console.log("player chose " + playerChoice + " computer chose " + computerChoice);
-         progressText.textContent = "Player chose " + playerChoice + ", Computer chose " + computerChoice;
+         //progressText.textContent = "Player chose " + playerChoice + ", Computer chose " + computerChoice;
          tallyPoints(getChosenItem(playerChoice, computerChoice));
+         //drawHtml.textContent = '';
        });
    }
-   
    let displayResult = document.getElementById("#displayResult");
    for (let item of items) {
         item.addEventListener('mouseenter', function (e) {
@@ -147,19 +150,11 @@
    function playAgain() {
         const playAgain = document.createElement('button');
             displayResult.insertAdjacentElement('afterend', playAgain);
-            playAgain.textContent = "Play Again?";
+            playAgain.textContent = "Try Again?";
             playAgain.style.marginLeft = 25;
             playAgain.style.padding = 35;
             playAgain.addEventListener('click', function () {
-            let playAgainAnswer = prompt("Do you want to play Again", 'No');
-            if (playAgainAnswer == 'yes') {
                 window.addEventListener('click', ()=>{ location.reload()});
-            } else if (playAgainAnswer == 'no') {
-                return;
-            } else {
-                alert("please enter Yes or No");
-            }
-            playAgainAnswer;
             })
    }
     
